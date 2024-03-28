e24c976800cf396c73fc79c97acc9a412199338a
Zane <zmcamp1306@muleriders.saumag.edu>
2024-03-28 16:34:28 -0500

    Dynamic Inventory Finished

--- Assets/Testing/ZaneTesting/InventoryTest/InventoryUI.cs
+++ Assets/Testing/ZaneTesting/InventoryTest/InventoryUI.cs
@@ -1,27 +1,39 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
     public Transform itemsParent;
     public GameObject inventoryUI;
-    
+
     InventorySlot[] slots;
-    private bool show = false;
+    public bool show = false;
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
         itemsParent.gameObject.SetActive(false);
 
         InventorySystem.Instance.onItemChangedCallBack += UpdateUI;
 
         slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        PostPlay();
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            Debug.Log("Hidden");
+            slots[i].ClearSlot();
+            slots[i].GetComponentInChildren<Image>().enabled = false;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(show)
+        if (show)
         {
             UpdateUI();
         }
@@ -33,24 +45,21 @@ public class InventoryUI : MonoBehaviour
         show = true;
     }
 
-    void UpdateUI ()
+    void UpdateUI()
     {
         for (int i = 0; i < slots.Length; i++)
         {
             if (i < InventorySystem.Instance.inventory.Count)
             {
-                if(slots[i].icon.enabled == false)
+                if (slots[i].icon.enabled == false)
                 {
-                    slots[i].icon.enabled = true;
+                    slots[i].GetComponentInChildren<Image>().enabled = true;
+                    Debug.Log("Revealed");
                 }
 
                 slots[i].Additem(InventorySystem.Instance.inventory[i].Data);
             }
-            else
-            {
-                slots[i].ClearSlot();
-                slots[i].icon.enabled = false;
-            }
+
         }
     }
 }
