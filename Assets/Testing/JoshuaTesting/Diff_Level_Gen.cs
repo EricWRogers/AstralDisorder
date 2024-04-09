ce3745dc1ea43dea259934ac2ab83e9c37b08f70
JTAdams <jtadams6501@gmail.com>
2024-04-09 16:43:46 -0500

    monster speed change with difficulty
    (incomplete)

---
+++ Assets/Testing/JoshuaTesting/Diff_Level_Gen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diff_Level_Gen : MonoBehaviour
+{
+    // Start is called before the first frame update 
+    //set up difficulty levels so the work where easy is 3 speed normal is 6 hard is 7 extreme is 9
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
